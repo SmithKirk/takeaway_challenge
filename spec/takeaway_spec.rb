e3f4df28eb9eefd710fb4e10c5a8bbd6bace7383
Kirk Smith <kirks@hotmail.co.uk>
2016-06-13 16:43:31 +0100

    when order is placed sms is sent

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,11 +1,16 @@
 describe Takeaway do
 
-  subject(:takeaway){described_class.new(menu: menu, order: order)}
+  subject(:takeaway){described_class.new(menu: menu, order: order, sms: sms, config: {})}
 
   let(:menu){instance_double('Menu', print_menu: printed_menu)}
   let(:order){instance_double('Order', total: 10.00)}
+  let(:sms){spy('SMS', deliver: nil)}
   let(:printed_menu){'Burger £3.00, Chips £1.50'}
   let(:dishes){['burger',2]}
+
+  before do
+    allow(order).to receive(:add)
+  end
 
   describe '#view_menu' do
     it 'responds to menu' do
@@ -25,11 +30,13 @@ describe Takeaway do
   end
 
   it 'knows order total' do
-    allow(order).to receive(:add)
     total = takeaway.place_order(dishes)
     expect(total).to eq (10.00)
   end
 
-
+  it 'sends an sms when order has been placed' do
+    takeaway.place_order(dishes)
+    expect(sms).to have_received(:deliver)
+  end
 
 end
